fa3b96a7473891f027f101ade35d30a69bd60aa5
Katie Lamber <katielamber02@gmail.com>
2020-01-21 15:22:46 +0200

    interface fro TodoFromProps

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import "materialize-css/dist/css/materialize.min.css";
 import { TodoForm } from "./components/TodoForm";
 
 const App: React.FC = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addHandler = (title: string) => {
+    console.log("Add todo", title);
+  };
   return (
     <>
       <Navbar />
       ME TEXT
       <div className="container">
-        <TodoForm />
+        <TodoForm onAdd={addHandler} />
       </div>
     </>
   );
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,16 +1,28 @@
 import React, { useRef } from "react";
 
-export const TodoForm: React.FC = () => {
+// without interface
+// const App: React.FC<{ onAdd(title: string): void }> = props => {
+
+//with interface:
+
+interface TodoFormProps {
+  onAdd(title: string): void;
+}
+
+export const TodoForm: React.FC<TodoFormProps> = props => {
   // const [title, setTitle] = useState<string>("");
   // const change = (event: React.ChangeEvent<HTMLInputElement>) => {
   //   setTitle(event.target.value);
   // };
 
   const myref = useRef<HTMLInputElement>(null);
+
   const keyPressHandler = (event: React.KeyboardEvent) => {
     if (event.key === "Enter") {
       // console.log(title);
       // setTitle("");
+
+      props.onAdd(myref.current!.value);
       console.log(myref.current!.value);
       myref.current!.value = "";
     }
