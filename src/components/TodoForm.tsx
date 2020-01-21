552872c58f669c911fd7cc2f747457e3a0f4194b
Katie Lamber <katielamber02@gmail.com>
2020-01-21 15:00:26 +0200

    useRef vs useState same input functionality

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,20 +1,26 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 
 export const TodoForm: React.FC = () => {
-  const [title, setTitle] = useState<string>("");
-  const change = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTitle(event.target.value);
-  };
+  // const [title, setTitle] = useState<string>("");
+  // const change = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setTitle(event.target.value);
+  // };
+
+  const myref = useRef<HTMLInputElement>(null);
   const keyPressHandler = (event: React.KeyboardEvent) => {
     if (event.key === "Enter") {
-      console.log(title);
+      // console.log(title);
+      // setTitle("");
+      console.log(myref.current!.value);
+      myref.current!.value = "";
     }
   };
   return (
     <div className="input-field mt2">
       <input
-        value={title}
-        onChange={change}
+        // value={title}
+        // onChange={change}
+        ref={myref}
         type="text"
         id="title"
         placeholder="put some text here"
